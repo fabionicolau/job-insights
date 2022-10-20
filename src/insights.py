fda6170b01b228d81701d084d824abe6d702c661
fabionicolau <fabionicolaujr@gmail.com>
2022-10-20 15:50:06 -0300

    feat: implementa a função get_unique_industries

--- src/insights.py
+++ src/insights.py
@@ -26,21 +26,13 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
+    table = read(path)
+    industries = []
+    for row in table:
+        if row["industry"] != "":
+            industries.append(row["industry"])
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    return list(set(industries))
 
 
 def filter_by_industry(jobs, industry):
