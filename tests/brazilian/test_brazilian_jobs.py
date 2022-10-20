76f1da1e3af64e928f41179f4d11145e1a58480e
fabionicolau <fabionicolaujr@gmail.com>
2022-10-20 19:32:20 -0300

    feat: implementa teste para a read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,7 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    table = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    for row in table:
+        assert ("title" in row) and ("salary" in row) and ("type" in row)
