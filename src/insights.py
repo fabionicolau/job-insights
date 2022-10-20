7457ab81f29d1bd2e9ba31a515ff115693d90406
fabionicolau <fabionicolaujr@gmail.com>
2022-10-20 17:05:59 -0300

    feat: implementa a função filter_by_industry

--- src/insights.py
+++ src/insights.py
@@ -4,6 +4,7 @@ from src.jobs import read
 def get_unique_job_types(path):
     table = read(path)
     job_types = [row["job_type"] for row in table]
+
     return list(set(job_types))
 
 
@@ -27,21 +28,12 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
+    filtered_industry = list()
+    for job in jobs:
+        if job["industry"] == industry:
+            filtered_industry.append(job)
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    return filtered_industry
 
 
 def get_max_salary(path):
